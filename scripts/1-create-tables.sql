e10950f57ad6d5e19483c4a5b70d6dc4093c6c1a
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2024-03-25 01:02:57 +0300

    On my way to checkpoint-7.

---
+++ scripts/1-create-tables.sql
@@ -0,0 +1,87 @@
+
+-- https://stackoverflow.com/a/31833279
+CREATE DOMAIN uint AS int4
+   CHECK(VALUE >= 0);
+
+
+CREATE TABLE Products (
+  id            UINT      PRIMARY KEY,
+  price_rub     UINT      NOT NULL,
+  registered_at TIMESTAMP NOT NULL,
+  released_at   TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Monitors (
+  id                   UINT        PRIMARY KEY,
+  product_id           UINT        NOT NULL UNIQUE,
+  model_line           VARCHAR(16) NULL,
+  model                VARCHAR(16) NOT NULL UNIQUE,
+  resolution_width_px  UINT        NOT NULL CHECK(resolution_width_px >= 1),
+  resolution_height_px UINT        NOT NULL CHECK(resolution_height_px >= 1),
+  diagonal_inches      FLOAT4      NOT NULL CHECK(diagonal_inches > 0),
+  glossy               BOOL        NOT NULL,
+  dc_dimming           BOOL        NOT NULL,
+
+  FOREIGN KEY (product_id) REFERENCES Products(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Cpus (
+  id                   UINT        PRIMARY KEY,
+  vendor               VARCHAR(16) NOT NULL CHECK(vendor = INITCAP(vendor)),
+  model                VARCHAR(32) NOT NULL CHECK(model = UPPER(model)),
+  cpubenchmark_pts     UINT        NOT NULL
+);
+
+CREATE TABLE Gpus (
+  id          UINT        PRIMARY KEY,
+  vendor      VARCHAR(16) NOT NULL CHECK(vendor = INITCAP(vendor)),
+  model       VARCHAR(16) NOT NULL CHECK(model = UPPER(model)),
+  furmark_fps UINT        NOT NULL
+);
+
+CREATE TABLE Monoblocks (
+  id              UINT        PRIMARY KEY,
+  product_id      UINT        NOT NULL UNIQUE,
+  model_line      VARCHAR(16) NULL,
+  model           VARCHAR(16) NOT NULL UNIQUE,
+  monitor_part_id UINT        NOT NULL,
+  cpu_id          UINT        NOT NULL,
+  gpu_id          UINT        NOT NULL,
+  ram_gb          UINT        NOT NULL CHECK(ram_gb > 0),
+
+  FOREIGN KEY (product_id) REFERENCES Products(id) ON DELETE CASCADE,
+  FOREIGN KEY (monitor_part_id) REFERENCES Monitors(id) ON DELETE RESTRICT,
+  FOREIGN KEY (cpu_id) REFERENCES Cpus(id) ON DELETE RESTRICT,
+  FOREIGN KEY (gpu_id) REFERENCES Gpus(id) ON DELETE RESTRICT
+);
+
+CREATE TABLE Users (
+  id                   UINT          PRIMARY KEY,
+  first_name           VARCHAR(32)   NOT NULL,
+  last_name            VARCHAR(32)   NOT NULL,
+  email                VARCHAR(128)  NOT NULL UNIQUE,
+  registered_at        TIMESTAMP     NOT NULL,
+  password_sha256_hash TEXT          NOT NULL,
+  yandex_oauth_token   TEXT          NULL
+);
+
+CREATE TABLE Reviews (
+  id         UINT      PRIMARY KEY,
+  product_id UINT      NOT NULL,
+  user_id    UINT      NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+
+  FOREIGN KEY (product_id) REFERENCES Products(id) ON DELETE RESTRICT,
+  FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE RESTRICT
+);
+
+CREATE TABLE ReviewRevs (
+  id          UINT      PRIMARY KEY,
+  review_id   UINT      NOT NULL,
+  content     TEXT      NOT NULL,
+  modified_at TIMESTAMP NOT NULL,
+
+  CONSTRAINT ReviewRevUq UNIQUE (review_id, modified_at),
+
+  FOREIGN KEY (review_id) REFERENCES Reviews(id) ON DELETE CASCADE
+);
